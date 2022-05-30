981ab7a65f548464325d59fcba1a088a2d12f640
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2022-05-30 05:30:44 +0300

    add player smooth rotation and camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform _target;
+    public float camSpeed;
+    public Vector3 offset;
+
+    //bu script sadece playerý takip etmesi için yazdým verdiðin offsetle takip eder
+
+    private void Update()
+    {
+        var target = _target.transform.position + offset;
+        var targetPos = new Vector3(transform.position.x, target.y, target.z);
+        var smoothPos = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * camSpeed);
+        transform.position = smoothPos;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,6 +21,34 @@ public class Player : MonoBehaviour
     LevelManager levelManager;
     Rigidbody rb;
     Animator animator;
+
+    public DirectionModule directionModule;
+
+
+    [Serializable]
+    public struct Range
+    {
+        public float min;
+        public float max;
+    }
+
+    [Serializable]
+    public struct DirectionModule
+    {
+
+        public float rotationSpeed; //bu abimiz rotasyon hýzý ne kadar hýzlý saða sola dönecek
+        public Range rotationLimit; //rotasyon limitleri ne kadar saða sola döncek
+        public Quaternion CurrentRotation { get; private set; } //bu abimiz sadece bizim o anki current rotationu tutuyo o anki rotation neyse bu da o
+        public bool IsLeft { get; set; } // solda mý saðda mý kontrol amaçlý isleft true gelirse minimumu false gelirse maxý kullanýyo
+
+        public void SmoothRotation()
+        {
+            var dt = Time.deltaTime;
+
+            var rotation = Quaternion.Euler(0, IsLeft ? rotationLimit.min : rotationLimit.max, 0);
+            CurrentRotation = Quaternion.Slerp(CurrentRotation, rotation,rotationSpeed * dt);
+        }
+    }
     void Start()
     {
         gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
@@ -47,17 +76,26 @@ public class Player : MonoBehaviour
         {
             Debug.Log("died");
         }
+
+        // bak þimdi gördüm bizim xposla iþimiz yok sanýrým
+        directionModule.SmoothRotation();
+        rb.MoveRotation(directionModule.CurrentRotation);
+
     }
     private Vector3  MovePosition()
     {
-        Vector3 moveVector = new Vector3(xPos * Time.deltaTime, 0, forwardSpeed * Time.deltaTime);
+        
+        Vector3 moveVector = transform.forward * forwardSpeed * Time.deltaTime;
+            //new Vector3(xPos * Time.deltaTime, 0, forwardSpeed * Time.deltaTime);
         return moveVector;
     }
 
     public void ChangeDirections()
     {
+        // burda islefti setliyoruz xpos eðer 0dan küçükse true geliyo büyükse false
         xPos *= -1;
-        playerTransform.rotation = Quaternion.Euler(0, (xPos <= -1) ? -125 : 125, 0);
+        directionModule.IsLeft = xPos < 0;
+        //playerTransform.rotation = Quaternion.Euler(0, (xPos <= -1) ? -125 : 125, 0);
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -91,3 +129,6 @@ public class Player : MonoBehaviour
         menuUI.SetActive(true);
     }
 }
+
+
+
